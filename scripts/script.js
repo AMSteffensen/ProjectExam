836d8bede30ca38c9b810d64a05362ac447b825d
Andreas Steffensen <andreas.steffensen89@gmail.com>
2018-12-01 17:41:43 +0100

    add map section

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,20 @@ const launchData = 'https://api.spacexdata.com/v2/launches/latest'
 fetch(launchData)
     .then(response => response.json())
     .then(data => {
-        console.log(data) 
+        console.log(data)
     })
     .catch(error => console.error(error))
+
+
+
+
+
+
+
+
+
+
+// Datepicker
+
+var dateControl = document.querySelector('input[type="date"]');
+dateControl.value = '2017-06-01';
