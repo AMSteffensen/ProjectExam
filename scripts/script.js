6e99c47fd70700bdcff6a82fe45ac906df5b34ca
Andreas Steffensen <andreas.steffensen89@gmail.com>
2018-12-07 23:04:43 +0100

    add flex layout tags

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,11 @@ dateControl.value = '2017-06-01';
 
 
 // on pageload run JS
-document.addEventListener("DOMContentLoaded", function () {
-
-    var sections = document.querySelectorAll(".section");
-    var navigation = document.querySelector("nav");
-    var links = navigation.querySelectorAll("a");
 
 
-
-    // Map
-    var headquarters = L.map('map').setView([51.505, -0.09], 13);
+   
+// navigation
+var toggle = document.getElementById("toggle").addEventListener("click", toggleNav);
 
 
 
@@ -22,18 +17,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-    // navigation
-    var toggle = document.querySelector('#toggle');
-    var menu = document.querySelector('#menu');
 
-    toggle.addEventListener('click', function () {
-        if (menu.classList.contains('is-active')) {
-            this.setAttribute('aria-expanded', 'false');
-            menu.classList.remove('is-active');
-        } else {
-            menu.classList.add('is-active');
-            this.setAttribute('aria-expanded', 'true');
-        }
-    });
-
-}, false);
