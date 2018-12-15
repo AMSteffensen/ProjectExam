2a214b33ad752261bb8d29cb36764583a6e60850
Andreas Steffensen <andreas.steffensen89@gmail.com>
2018-12-15 21:24:51 +0100

    add display data to content listing data

--- scripts/about.js
+++ scripts/about.js
@@ -1,35 +1,31 @@
-const info = 'https://api.spacexdata.com/v3/info';
-const content = document.getElementById('content');
+var info = 'https://api.spacexdata.com/v3/info';
 
 function getData() {
-  fetch(info)
-    .then(result => result.json())
-    .then((res) => {
-      displayData();
-    })
+  fetch(info).then(function (result) {
+    return result.json();
+  }).then(function (res) {
+    displayData(res);
+  }).catch(function (err) {
+    return console.log(err);
+  });
 }
 
-function displayHistory() {
-  content.innerHTML = "";
-  content.innerHTML = 'History';
-}
+function displayData(result) {
 
-
-function displayRockets() {
-  content.innerHTML = "";
-  content.innerHTML = 'Rockets'
-}
-
-function displayData() {
+  var details = document.getElementById('content');
+  details.innerHTML += "<span class='list-group-item'><strong>Name:</strong> " + result.name + "</span>"+"<br />";
+  details.innerHTML += "<span class='list-group-item'><strong>Founder:</strong>   " + result.founder + "</span>"+"<br />";
+  details.innerHTML += "<span class='list-group-item'><strong>Founded:</strong> " + result.founded + "</span>"+"<br />";
+  details.innerHTML += "<span class='list-group-item'><strong>Employees:</strong>" + result.employees + "</span>"+"<br />";
+  details.innerHTML += "<span class='list-group-item'><strong>Vehicles:</strong>   " + result.vehicles + "</span>"+"<br />";
+  details.innerHTML += "<span class='list-group-item'><strong>Launch Sites:   </strong>" + result.launch_sites + "</span>"+"<br />";
+  details.innerHTML += "<span class='list-group-item'><strong>Test Sites:  </strong> " + result.test_sites + "</span>"+"<br />";
+  details.innerHTML += "<span class='list-group-item'><strong>CEO: </strong>  " + result.ceo + "</span>"+"<br />";
+  details.innerHTML += "<span class='list-group-item'><strong>COO:  </strong> " + result.coo + "</span>"+"<br />";
+  details.innerHTML += "<span class='list-group-item'><strong>CTO Propulsion:  </strong> " + result.cto_propulsion + "</span>"+"<br />";
+  details.innerHTML += "<span class='list-group-item'><strong>Valuation: </strong>  " + result.valuation + "</span>"+"<br />";
   /* 
-        "name": "SpaceX",
-    "founder": "Elon Musk",
-    "founded": 2002,
-    "employees": 7000,
-    "vehicles": 3,
-    "launch_sites": 3,
-    "test_sites": 1,
-    "ceo": "Elon Musk",
+ 
     "cto": "Elon Musk",
     "coo": "Gwynne Shotwell",
     "cto_propulsion": "Tom Mueller",
@@ -46,24 +42,30 @@ function displayData() {
         "elon_twitter": "https://twitter.com/elonmusk"
     },
     "summary": "SpaceX designs, manufactures and launches advanced rockets and spacecraft. The company was founded in 2002 to revolutionize space technology, with the ultimate goal of enabling people to live on other planets.
+         */
+}
 
-        */
+function displayHistory() {
+  content.innerHTML = "";
+  content.innerHTML = 'History';
+}
+
+function displayRockets() {
+  content.innerHTML = "";
+  content.innerHTML = 'Rockets';
 }
 
 
 function addEventListnerer() {
-var historyTrigger = document.getElementById("historyTrigger");
-historyTrigger.addEventListener("click", displayHistory);
+  var historyTrigger = document.getElementById("historyTrigger");
+  historyTrigger.addEventListener("click", displayHistory);
 
-var rocketsTrigger = document.getElementById("rocketsTrigger");
-rocketsTrigger.addEventListener("click", displayRockets);
-
+  var rocketsTrigger = document.getElementById("rocketsTrigger");
+  rocketsTrigger.addEventListener("click", displayRockets);
 }
-
 
 // Make self invoking function for loading scripts
 (function () {
-  "use strict";
   getData();
   addEventListnerer();
 })();
